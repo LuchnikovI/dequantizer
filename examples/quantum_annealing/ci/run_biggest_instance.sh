be002f4dd72676ed8b8b85ce5bd3b53986d5b3b6
Ilia Luchnikov <luchnikovilya@gmail.com>
2024-05-07 15:22:56 +0400

    biggest instance experimen

---
+++ examples/quantum_annealing/ci/run_biggest_instance.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#SBATCH --mail-user=ilia.luchnikov@tii.ae
+#SBATCH --job-name=bp_quantum_annealing
+#SBATCH --nodes=1
+#SBATCH --time=4-00:00:00
+#SBATCH --mem=128GB
+#SBATCH --mail-type=END,FAIL
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+. "${script_dir}/runner.sh" \
+    task_generator=very_large_random_regular \
+    quantum_annealing_schedule=linear_wo_sampling_very_long
